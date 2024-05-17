4b05ebbdde8eabf912e018fee91388a85f8e15b6
Alejandro Sánchez <19179333+AlexSnipes@users.noreply.github.com>
2024-05-17 08:25:37 -0300

    Update bernoulli.py

--- bernoulli.py
+++ bernoulli.py
@@ -1,6 +1,10 @@
 '''
 Bernoulli
 '''
+# Convertir velocidades de km/h a m/s
+def kmh_to_ms(kmh):
+    return float(kmh) * 1000 / 3600
+    
 # Definir constantes
 rho = 0.125  # Densidad del aire en kgf * s^2/m^4
 P0 = 10330  # Presión atmosférica a nivel del mar en kg/m^2
@@ -20,5 +24,5 @@ P_ext = P0 + 0.5 * rho * (v0**2 - v_ext**2)
 P_int = P0 + 0.5 * rho * (v0**2 - v_int**2)
 
 # Imprimir los resultados
-print("Presión en el extradós: ", P_ext, " kg/m^2")
-print("Presión en el intradós: ", P_int, " kg/m^2")
+print("Presión en el extradós: ", round(P_ext, 2), " kg/m^2")
+print("Presión en el intradós: ", round(P_int, 2), " kg/m^2")
